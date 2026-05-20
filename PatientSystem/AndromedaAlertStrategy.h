b0fd321fd91a811f0ee95cd914deafd36b014837
bachie07 <giabach0704@gmail.com>
2026-05-20 22:14:57 +1000

    implement andromeda and keprel header file and create their cpp files

--- PatientSystem/AndromedaAlertStrategy.h
+++ PatientSystem/AndromedaAlertStrategy.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "AlertStrategy.h"
+
+class AndromedaAlertStrategy : public AlertStrategy {
+
+public:
+	AlertLevel calculate(const Patient& patient, const Vitals& vitals) override;
+};
--- PatientSystem/KepralAlertStrategy.h
+++ PatientSystem/KepralAlertStrategy.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "AlertStrategy.h"
+
+class KepralAlertStrategy : public AlertStrategy {
+
+public:
+	AlertLevel calculate(const Patient& patient, const Vitals& vitals) override;
+};
