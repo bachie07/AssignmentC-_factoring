ffdaf332b2b622b7ce216e94f4479f95220d9b69
bachie07 <giabach0704@gmail.com>
2026-05-20 21:17:25 +1000

    comments for AlertStrategy header

--- PatientSystem/AlertStrategy.h
+++ PatientSystem/AlertStrategy.h
@@ -8,8 +8,11 @@ class AlertStrategy {
 
 public:
 
+	//pure virtual - the different strategies will implement this
+
 	virtual void calculate(const Patient& patient, const Vitals& vitals) = 0;
 
+	//virtual destructor
 	virtual ~AlertStrategy() = default;
 
 };
