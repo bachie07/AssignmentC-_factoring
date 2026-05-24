d4c294936b028e700d75b7de3fa233efdf96f8a9
bachie07 <giabach0704@gmail.com>
2026-05-24 22:38:00 +1000

    add patientfileloader adpater header and cpp

--- PatientSystem/PatientFileLoaderAdapter.h
+++ PatientSystem/PatientFileLoaderAdapter.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "AbstractPatientDatabaseLoader.h"
+#include "PatientFileLoader.h"
+
+class PatientFileLoaderAdapter : public AbstractPatientDatabaseLoader {
+
+public:
+
+	PatientFileLoaderAdapter(const std::string& filename);
+
+	void initialiseConnection() override;
+
+	void loadPatients(std::vector<Patient*>& patientIn) override;
+
+	void closeConnection() override;
+
+private:
+	PatientFileLoader _fileLoader;
+	std::string _filename;
+
+};
