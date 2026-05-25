631dd20c3303d77f7f6f9800e3aff226ef76330d
bachie07 <giabach0704@gmail.com>
2026-05-25 11:52:48 +1000

    add partial commenting for files

--- PatientSystem/AndromedaAlertStrategy.cpp
+++ PatientSystem/AndromedaAlertStrategy.cpp
@@ -3,20 +3,20 @@
 #include "Vitals.h"
 #include "PatientAlertLevels.h"
 
+//calculate alert level based on blood pressure as per table 1
 AlertLevel AndromedaAlertStrategy::calculate(const Patient& patient, const Vitals& vitals) {
 
-	if (vitals.BP() > 140) {
+	if (vitals.BP() > 140) { // if bp > 140 = red
 		return AlertLevel::Red;
 	}
-	else if (vitals.BP() > 130) {
+	else if (vitals.BP() > 130) { // if bp > 130 = orange
 
 		return AlertLevel::Orange;
 	}
-	else if (vitals.BP() > 110) {
+	else if (vitals.BP() > 110) { // if bp > 110 = yellow
 		
 		return AlertLevel::Yellow;
 	}
 
-	return AlertLevel::Green;
-
+	return AlertLevel::Green; // otherwise green
 }
--- PatientSystem/CordycepsAlertStrategy.h
+++ PatientSystem/CordycepsAlertStrategy.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "AlertStrategy.h"
 
+// strategy for Cordyceps brain infection
 class CordycepsAlertStrategy : public AlertStrategy {
 
+	// implement alert level calculation based on respiratory rate
 public:
 	AlertLevel calculate(const Patient& patient, const Vitals& vitals) override;
 };
