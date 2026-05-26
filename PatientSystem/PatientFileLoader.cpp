6cfdd6e92511217c6211f73d3f4d32259da8b13c
bachie07 <giabach0704@gmail.com>
2026-05-26 18:18:53 +1000

    load patients file explanation

--- PatientSystem/PatientFileLoader.cpp
+++ PatientSystem/PatientFileLoader.cpp
@@ -19,22 +19,24 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
 {
 	vector<Patient*> patients{};
 
-    std::ifstream inFile(file);
-    if (inFile.is_open()) {
+    std::ifstream inFile(file); 
+    if (inFile.is_open()) { // file checks
 
         string line;
         
-        while (getline(inFile, line)) {
+        while (getline(inFile, line)) { // reads line by line
 
             stringstream ss(line);
             string uid, fullName, birthday, disease, vitalsStr;
 
+            // get line at |, storing it in the specific variable
             getline(ss, uid, '|');
             getline(ss, fullName, '|');
             getline(ss, birthday, '|');
             getline(ss, disease, '|');
             getline(ss, vitalsStr);
 
+            // same thing for name
             stringstream nameSS(fullName);
 
             string lastName, firstName;
@@ -43,17 +45,18 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
             getline(nameSS, firstName, ',');
 
 
+
             std::tm t{};
 
             std::istringstream dateSS(birthday);
 
-            dateSS >> std::get_time(&t, "%d-%m-%Y");
+            dateSS >> std::get_time(&t, "%d-%m-%Y"); // read birthday string and pass into c++ time structure
 
-            Patient* p = new Patient(firstName, lastName, t);
+            Patient* p = new Patient(firstName, lastName, t); // instantiate the patient object
 
-            p->addDiagnosis(disease);
+            p->addDiagnosis(disease); // add diagnosis to patient object
 
-            if (!vitalsStr.empty()) {
+            if (!vitalsStr.empty()) { // check if theres vital data
 
                 stringstream vitalsSS(vitalsStr);
                 string vitalToken;
@@ -63,20 +66,21 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
                     stringstream vSS(vitalToken);
                     string bt, bp, hr, rr;
 
+                    // split value into metrics
                     getline(vSS, bt, ',');
                     getline(vSS, bp, ',');
                     getline(vSS, hr, ',');
                     getline(vSS, rr, ',');
 
-                    Vitals* v = new Vitals(stof(bt), stoi(bp), stoi(hr), stoi(rr));
+                    Vitals* v = new Vitals(stof(bt), stoi(bp), stoi(hr), stoi(rr)); // store in Vitals object, stof convert to float, stof convert to int
 
-                    p->addVitals(v);
+                    p->addVitals(v); // add vitals to patients object
 
 
                 }
             }
                 
-            patients.push_back(p);
+            patients.push_back(p); // store in patients vector
          
 
         }
